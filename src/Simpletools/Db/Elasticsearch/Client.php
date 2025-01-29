ee40fad5869145a2c8a57e555bbeccf7342fdfd6
Ciaran Moore <ciaran@feedoptimise.com>
2025-01-29 09:00:01 +0000

    revert mixed amendments

--- src/Simpletools/Db/Elasticsearch/Client.php
+++ src/Simpletools/Db/Elasticsearch/Client.php
@@ -14,7 +14,7 @@ class Client
     protected $___connection;
 
 
-    public function __construct(mixed $cluster=null)
+    public function __construct(string|null $cluster=null)
     {
         if($cluster)
             $this->___cluster = $cluster;
@@ -106,10 +106,10 @@ class Client
 
 		public function execute($endpoint, 
 			$method = 'GET',
-			mixed $data = null,
+			string|array|null $data = null,
 			$contentType ='application/json',
 			$attempt=0, 
-			mixed $retryPoints=null)
+			string|array|null $retryPoints=null)
 		{
 			$settings = self::$_gSettings[self::$_defaultCluster];
 
--- src/Simpletools/Db/Elasticsearch/Model.php
+++ src/Simpletools/Db/Elasticsearch/Model.php
@@ -7,7 +7,7 @@ class Model extends Client
     protected static $____selfModel;
     protected $___cluster;
 
-    public function __construct(mixed $cluster=null)
+    public function __construct(string|null $cluster=null)
     {
         $this->___cluster 	= defined('static::CLUSTER') ? static::CLUSTER : $cluster;
 
@@ -39,7 +39,7 @@ class Model extends Client
         return $query;
     }
 
-		public function doc(mixed $id =null,mixed $index = null)
+		public function doc(string|array|null $id =null,string|null $index = null)
 		{
 			return new Doc($id, $index);
 		}
